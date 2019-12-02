1a5ac58608ebb0a974d6925177b770ce48411f0b
JC Holder <jc.holder@thoughtworks.com>
2019-12-02 14:20:09 -0500

    Complete initial (failing) tests

--- 01/fuel/src/main.rs
+++ 01/fuel/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn fuel_required(mass: Mass) -> Mass {
+fn fuel_required(_mass: Mass) -> Mass {
     0
 }
 
@@ -15,5 +15,20 @@ mod tests {
     #[test]
     fn mass_12() {
         assert_eq!(fuel_required(12), 2);
+    }
+
+    #[test]
+    fn mass_14() {
+        assert_eq!(fuel_required(14), 2);
+    }
+
+    #[test]
+    fn mass_1969() {
+        assert_eq!(fuel_required(1969), 654);
+    }
+
+    #[test]
+    fn mass_100756() {
+        assert_eq!(fuel_required(100756), 33583);
     }
 }
