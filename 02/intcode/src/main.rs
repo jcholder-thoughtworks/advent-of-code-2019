dd01cd6390920e9ad421a9b7d0e4a3216216ae5c
JC Holder <jc.holder@thoughtworks.com>
2019-12-02 17:57:02 -0500

    Add additional tests

--- 02/intcode/src/main.rs
+++ 02/intcode/src/main.rs
@@ -52,4 +52,40 @@ mod tests {
 
         assert_eq!(expected, execute_intcode(program, 0));
     }
+
+    #[test]
+    fn small_program_1() {
+        let program = vec![1,0,0,0,99];
+
+        let expected = vec![2,0,0,0,99];
+
+        assert_eq!(expected, execute_intcode(program, 0));
+    }
+
+    #[test]
+    fn small_program_2() {
+        let program = vec![2,3,0,3,99];
+
+        let expected = vec![2,3,0,6,99];
+
+        assert_eq!(expected, execute_intcode(program, 0));
+    }
+
+    #[test]
+    fn small_program_3() {
+        let program = vec![2,4,4,5,99,0];
+
+        let expected = vec![2,4,4,5,99,9801];
+
+        assert_eq!(expected, execute_intcode(program, 0));
+    }
+
+    #[test]
+    fn small_program_4() {
+        let program = vec![1,1,1,4,99,5,6,0,99];
+
+        let expected = vec![30,1,1,4,2,5,6,0,99];
+
+        assert_eq!(expected, execute_intcode(program, 0));
+    }
 }
