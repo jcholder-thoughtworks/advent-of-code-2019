86f727f2a28c5dabe27384645be65794456aff1b
JC Holder <jc.holder@thoughtworks.com>
2019-12-02 14:17:38 -0500

    Initial (failing) test

--- 01/fuel/src/main.rs
+++ 01/fuel/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn fuel_required(mass: i32) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mass_12() {
+        assert_eq!(fuel_required(12), 2);
+    }
+}
