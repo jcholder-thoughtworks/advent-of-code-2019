37c9c82ea716b4b644a38d360390f2bea8c55958
JC Holder <jc.holder@thoughtworks.com>
2019-12-02 16:46:45 -0500

    Preliminary program setup

--- 02/intcode/src/main.rs
+++ 02/intcode/src/main.rs
@@ -1,3 +1,24 @@
+type IntcodeProgram = Vec<i32>;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn execute_intcode(ints: IntcodeProgram) -> IntcodeProgram {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn minimal_program() {
+        let program = vec![1, 0, 0, 0, 99];
+
+        let expected = vec![2, 0, 0, 0, 99];
+        let actual = execute_intcode(program);
+
+        assert_eq!(expected, actual);
+    }
 }
