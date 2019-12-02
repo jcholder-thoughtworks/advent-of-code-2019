7bdbb0989f878e7fbaf88d2d985062e7f9e3d97c
JC Holder <jc.holder@thoughtworks.com>
2019-12-02 15:00:06 -0500

    Separate out naive fuel calculation code

--- 01/fuel/src/main.rs
+++ 01/fuel/src/main.rs
@@ -7,7 +7,18 @@ type Mass = u32;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+
+    // for part 1, when the added fuel is not factored into the grand total
+    let naive: bool;
+    let filename: &String;
+
+    if &args[1] == "--naive" {
+        naive = true;
+        filename = &args[2];
+    } else {
+        naive = false;
+        filename = &args[1];
+    }
 
     let file = File::open(filename)?;
     let file = BufReader::new(file);
@@ -18,8 +29,12 @@ fn main() -> io::Result<()> {
 
     for line in lines_iter {
         let module_mass: Mass = line.parse().unwrap();
-        total_fuel += fuel_required(module_mass);
-        // println!("{:?}", fuel_required(parsed));
+        if naive {
+            total_fuel += naive_fuel_required(module_mass);
+        } else {
+            total_fuel += naive_fuel_required(module_mass);
+        }
+        // println!("{:?}", naive_fuel_required(parsed));
     }
 
     println!("Total fuel required: {:?}", total_fuel);
@@ -27,7 +42,7 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn fuel_required(mass: Mass) -> Mass {
+fn naive_fuel_required(mass: Mass) -> Mass {
     (mass / 3) - 2
 }
 
@@ -36,22 +51,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn mass_12() {
-        assert_eq!(fuel_required(12), 2);
+    fn mass_12_naive() {
+        assert_eq!(naive_fuel_required(12), 2);
     }
 
     #[test]
-    fn mass_14() {
-        assert_eq!(fuel_required(14), 2);
+    fn mass_14_naive() {
+        assert_eq!(naive_fuel_required(14), 2);
     }
 
     #[test]
-    fn mass_1969() {
-        assert_eq!(fuel_required(1969), 654);
+    fn mass_1969_naive() {
+        assert_eq!(naive_fuel_required(1969), 654);
     }
 
     #[test]
-    fn mass_100756() {
-        assert_eq!(fuel_required(100756), 33583);
+    fn mass_100756_naive() {
+        assert_eq!(naive_fuel_required(100756), 33583);
     }
 }
