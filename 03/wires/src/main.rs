253d00b678d0b7b620c991c5af980eaa51cda4f0
JC Holder <jc.holder@thoughtworks.com>
2019-12-03 11:56:44 -0500

    Add and use WireDirection enum. Add WD alias

--- 03/wires/src/main.rs
+++ 03/wires/src/main.rs
@@ -2,14 +2,25 @@ fn main() {
     println!("Hello, world!");
 }
 
+enum WireDirection {
+    U(i32), // Up
+    R(i32), // Right
+    D(i32), // Down
+    L(i32), // Left
+}
+
+type WD = WireDirection;
+
+type Wire = Vec<WireDirection>;
+
 #[cfg(test)]
 pub mod tests {
     use super::*;
 
     #[test]
     fn minimal_program() {
-        let wire1 = ["R10", "U10"];
-        let wire2 = ["U5", "R15"];
+        let wire1 = [WD::R(10), WD::U(10)];
+        let wire2 = [WD::U(5), WD::R(15)];
 
         assert_eq!(closest_intersection_distance(wire1, wire2), 15);
     }
