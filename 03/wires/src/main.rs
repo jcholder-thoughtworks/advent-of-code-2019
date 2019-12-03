f1d3994ac5e2a17a6ed8c9028b9256f874814d72
JC Holder <jc.holder@thoughtworks.com>
2019-12-03 11:49:26 -0500

    Draft initial test

--- 03/wires/src/main.rs
+++ 03/wires/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn minimal_program() {
+        let wire1 = ["R10", "U10"];
+        let wire2 = ["U5", "R15"];
+
+        assert_eq!(closest_intersection_distance(wire1, wire2), 15);
+    }
+}
