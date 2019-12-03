3b20f829c000da16abc9341f6bac728684147da5
JC Holder <jc.holder@thoughtworks.com>
2019-12-03 12:03:42 -0500

    Placeholder implementation of #closest_intersection_distance. Add Distance alias

--- 03/wires/src/main.rs
+++ 03/wires/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     println!("Hello, world!");
 }
 
+type Distance = i32;
+
 enum WireDirection {
     U(i32), // Up
     R(i32), // Right
@@ -13,15 +15,19 @@ type WD = WireDirection;
 
 type Wire = Vec<WireDirection>;
 
+fn closest_intersection_distance(wire1: &Wire, wire2: &Wire) -> Distance {
+    0
+}
+
 #[cfg(test)]
 pub mod tests {
     use super::*;
 
     #[test]
     fn minimal_program() {
-        let wire1 = [WD::R(10), WD::U(10)];
-        let wire2 = [WD::U(5), WD::R(15)];
+        let wire1 = vec![WD::R(10), WD::U(10)];
+        let wire2 = vec![WD::U(5), WD::R(15)];
 
-        assert_eq!(closest_intersection_distance(wire1, wire2), 15);
+        assert_eq!(closest_intersection_distance(&wire1, &wire2), 15);
     }
 }
