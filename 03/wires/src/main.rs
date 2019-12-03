25372caaed23fc7391364f4cd647e74bd405ed4b
JC Holder <jc.holder@thoughtworks.com>
2019-12-03 16:48:01 -0500

    Implement binary for problem solver

--- 03/wires/src/main.rs
+++ 03/wires/src/main.rs
@@ -1,7 +1,20 @@
 use std::convert::From;
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+    let file = BufReader::new(file);
+
+    let lines: Vec<String> = file.lines().map(|l| l.unwrap()).collect();
+
+    let wire1 = Wire::from(lines[0].as_str());
+    let wire2 = Wire::from(lines[1].as_str());
+
+    println!("Distance: {}", wire1.closest_intersection_distance_with(&wire2));
+
+    Ok(())
 }
 
 type Distance = i32;
