63325b5f00b019c2dae5ade9edcadfa9ef91fc1a
JC Holder <jc.holder@thoughtworks.com>
2019-12-03 12:23:13 -0500

    WIP: Derive Wire from WireInstructions

--- 03/wires/src/main.rs
+++ 03/wires/src/main.rs
@@ -4,12 +4,16 @@ fn main() {
 
 type Distance = i32;
 
+#[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
 }
 
+#[derive(Debug)]
 struct Segment(Point, Point);
+
+type Wire = Vec<Segment>;
 
 enum WireDirection {
     U(i32), // Up
@@ -22,8 +26,41 @@ type WD = WireDirection;
 
 type WireInstructions = Vec<WireDirection>;
 
-fn closest_intersection_distance(wire1: &WireInstructions, wire2: &WireInstructions) -> Distance {
+fn closest_intersection_distance(
+    wire_instructions1: &WireInstructions,
+    wire_instructions2: &WireInstructions
+    ) -> Distance {
+
+    println!("Test output: {:?}", instructions_to_wire(wire_instructions1));
+    println!("Test output: {:?}", instructions_to_wire(wire_instructions2));
+
     0
+}
+
+fn instructions_to_wire(wire_instructions: &WireInstructions) -> Wire {
+    let mut wire: Wire = vec![];
+
+    let mut x = 0;
+    let mut y = 0;
+
+    for direction in wire_instructions {
+        let start = Point { x, y };
+        let end = match direction {
+            WD::U(distance) => Point { x, y: y + distance },
+            WD::R(distance) => Point { x: x + distance, y },
+            WD::D(distance) => Point { x, y: y - distance },
+            WD::L(distance) => Point { x: x - distance, y },
+        };
+
+        x = end.x;
+        y = end.y;
+
+        let new_segment = Segment(start, end);
+
+        wire.push(new_segment);
+    }
+
+    wire
 }
 
 #[cfg(test)]
