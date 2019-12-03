79d0d176df3d1539fd6d67ea7b2a4a442351ce61
JC Holder <jc.holder@thoughtworks.com>
2019-12-03 11:16:23 -0500

    Implement deduction of target output

--- 02/intcode/src/bin/deduce.rs
+++ 02/intcode/src/bin/deduce.rs
@@ -5,12 +5,31 @@ use intcode::*;
 
 fn main() -> io::Result<()> {
     let code = fs::read_to_string("input.txt")?;
-    let program = parse_code(code);
+    let original_program = parse_code(code);
 
-    let original_program = execute_intcode(program.to_vec());
+    let (noun, verb) = deduce_required_inputs(original_program.to_vec());
+    let combination = 100 * noun + verb;
 
-    println!("Input: {:?}", program);
-    println!("Original output: {:?}", original_program);
+    println!("Solution found: {:1}, {:2}", noun, verb);
+    println!("Combination: {}", combination);
 
     Ok(())
+}
+
+fn deduce_required_inputs(original_program: IntcodeProgram) -> (i32, i32) {
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut trial_program = original_program.to_vec();
+            trial_program[NOUN] = noun;
+            trial_program[VERB] = verb;
+
+            let executed = execute_intcode(trial_program);
+
+            if executed[OUTPUT] == 19690720 {
+                return (executed[NOUN], executed[VERB])
+            }
+        }
+    }
+
+    panic!("Solution not found");
 }
