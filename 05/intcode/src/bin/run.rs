297a8f66ec9a586ca52967cd73a07c51b5391de5
JC Holder <jc.holder@thoughtworks.com>
2019-12-05 11:26:33 -0500

    Rename IntcodeProgram to Intcode in anticipation of adding a Program struct. Rename variables accordingly

--- 05/intcode/src/bin/run.rs
+++ 05/intcode/src/bin/run.rs
@@ -5,13 +5,13 @@ use intcode::*;
 
 fn main() -> io::Result<()> {
     let code = fs::read_to_string("input.txt")?;
-    let original_program = parse_code(code);
+    let original_intcode = parse_code(code);
 
     let input = 0; // example input
 
     println!("Input: {}", input);
 
-    let (_executed_program, output) = execute_intcode(original_program, input);
+    let (_executed_intcode, output) = execute_intcode(original_intcode, input);
     println!("Output: {}", output);
 
     Ok(())
--- 05/intcode/src/lib.rs
+++ 05/intcode/src/lib.rs
@@ -1,4 +1,4 @@
-pub type IntcodeProgram = Vec<i32>;
+pub type Intcode = Vec<i32>;
 pub type Pointer = usize;
 pub type Input = i32;
 pub type Output = i32;
@@ -7,55 +7,55 @@ pub const OUTPUT: Pointer = 0;
 pub const NOUN: Pointer = 1;
 pub const VERB: Pointer = 2;
 
-pub fn parse_code(code: String) -> IntcodeProgram {
-    let program = code.trim().split(',');
-    program.map(|c| c.parse().unwrap()).collect()
+pub fn parse_code(code: String) -> Intcode {
+    let intcode = code.trim().split(',');
+    intcode.map(|c| c.parse().unwrap()).collect()
 }
 
-pub fn execute_intcode(program: IntcodeProgram, input: Input) -> (IntcodeProgram, Output) {
-    execute_intcode_at_pointer(program, 0, input)
+pub fn execute_intcode(intcode: Intcode, input: Input) -> (Intcode, Output) {
+    execute_intcode_at_pointer(intcode, 0, input)
 }
 
-fn execute_intcode_at_pointer(mut program: IntcodeProgram, pointer: Pointer, input: Input) -> (IntcodeProgram, Output) {
-    let command = program[pointer];
+fn execute_intcode_at_pointer(mut intcode: Intcode, pointer: Pointer, input: Input) -> (Intcode, Output) {
+    let command = intcode[pointer];
 
     if command == 99 {
-        return (program, 0);
+        return (intcode, 0);
     }
 
     match command {
-        1 => perform_add(&mut program, pointer),
-        2 => perform_multiply(&mut program, pointer),
+        1 => perform_add(&mut intcode, pointer),
+        2 => perform_multiply(&mut intcode, pointer),
         _ => panic!("Unrecognized command: {:?}", command),
     };
 
-    execute_intcode_at_pointer(program, pointer + 4, input)
+    execute_intcode_at_pointer(intcode, pointer + 4, input)
 }
 
-fn perform_add(program: &mut IntcodeProgram, pointer: Pointer) {
-    let left_index = program[pointer + 1] as usize;
-    let right_index = program[pointer + 2] as usize;
-    let destination = program[pointer + 3] as usize;
+fn perform_add(intcode: &mut Intcode, pointer: Pointer) {
+    let left_index = intcode[pointer + 1] as usize;
+    let right_index = intcode[pointer + 2] as usize;
+    let destination = intcode[pointer + 3] as usize;
 
-    let left_value = program[left_index];
-    let right_value = program[right_index];
+    let left_value = intcode[left_index];
+    let right_value = intcode[right_index];
 
     let new_value = left_value + right_value;
 
-    program[destination] = new_value;
+    intcode[destination] = new_value;
 }
 
-fn perform_multiply(program: &mut IntcodeProgram, pointer: Pointer) {
-    let left_index = program[pointer + 1] as usize;
-    let right_index = program[pointer + 2] as usize;
-    let destination = program[pointer + 3] as usize;
+fn perform_multiply(intcode: &mut Intcode, pointer: Pointer) {
+    let left_index = intcode[pointer + 1] as usize;
+    let right_index = intcode[pointer + 2] as usize;
+    let destination = intcode[pointer + 3] as usize;
 
-    let left_value = program[left_index];
-    let right_value = program[right_index];
+    let left_value = intcode[left_index];
+    let right_value = intcode[right_index];
 
     let new_value = left_value * right_value;
 
-    program[destination] = new_value;
+    intcode[destination] = new_value;
 }
 
 
@@ -66,79 +66,79 @@ pub mod tests {
     const DEFAULT_INPUT: Input = 0;
 
     #[test]
-    fn minimal_program() {
-        let program = vec![1, 0, 0, 0, 99];
+    fn minimal_intcode() {
+        let intcode = vec![1, 0, 0, 0, 99];
 
         let expected = vec![2, 0, 0, 0, 99];
 
-        let (executed_program, _output) = execute_intcode(program, DEFAULT_INPUT);
+        let (executed_intcode, _output) = execute_intcode(intcode, DEFAULT_INPUT);
 
-        assert_eq!(expected, executed_program);
+        assert_eq!(expected, executed_intcode);
     }
 
     #[test]
-    fn example_program() {
-        let program = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+    fn example_intcode() {
+        let intcode = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
 
         let expected = vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
 
-        let (executed_program, _output) = execute_intcode(program, DEFAULT_INPUT);
+        let (executed_intcode, _output) = execute_intcode(intcode, DEFAULT_INPUT);
 
-        assert_eq!(expected, executed_program);
+        assert_eq!(expected, executed_intcode);
     }
 
     #[test]
-    fn small_program_1() {
-        let program = vec![1,0,0,0,99];
+    fn small_intcode_1() {
+        let intcode = vec![1,0,0,0,99];
 
         let expected = vec![2,0,0,0,99];
 
-        let (executed_program, _output) = execute_intcode(program, DEFAULT_INPUT);
+        let (executed_intcode, _output) = execute_intcode(intcode, DEFAULT_INPUT);
 
-        assert_eq!(expected, executed_program);
+        assert_eq!(expected, executed_intcode);
     }
 
     #[test]
-    fn small_program_2() {
-        let program = vec![2,3,0,3,99];
+    fn small_intcode_2() {
+        let intcode = vec![2,3,0,3,99];
 
         let expected = vec![2,3,0,6,99];
 
-        let (executed_program, _output) = execute_intcode(program, DEFAULT_INPUT);
+        let (executed_intcode, _output) = execute_intcode(intcode, DEFAULT_INPUT);
 
-        assert_eq!(expected, executed_program);
+        assert_eq!(expected, executed_intcode);
     }
 
     #[test]
-    fn small_program_3() {
-        let program = vec![2,4,4,5,99,0];
+    fn small_intcode_3() {
+        let intcode = vec![2,4,4,5,99,0];
 
         let expected = vec![2,4,4,5,99,9801];
 
-        let (executed_program, _output) = execute_intcode(program, DEFAULT_INPUT);
+        let (executed_intcode, _output) = execute_intcode(intcode, DEFAULT_INPUT);
 
-        assert_eq!(expected, executed_program);
+        assert_eq!(expected, executed_intcode);
     }
 
     #[test]
-    fn small_program_4() {
-        let program = vec![1,1,1,4,99,5,6,0,99];
+    fn small_intcode_4() {
+        let intcode = vec![1,1,1,4,99,5,6,0,99];
 
         let expected = vec![30,1,1,4,2,5,6,0,99];
 
-        let (executed_program, _output) = execute_intcode(program, DEFAULT_INPUT);
+        let (executed_intcode, _output) = execute_intcode(intcode, DEFAULT_INPUT);
 
-        assert_eq!(expected, executed_program);
+        assert_eq!(expected, executed_intcode);
     }
 
     #[test]
     #[ignore]
     fn basic_input_output() {
-        let program = vec![3,0,4,0,99];
+        let intcode = vec![3,0,4,0,99];
 
         let input = 5;
 
-        let (_, output) = execute_intcode(program, input);
+        let (_, output) = execute_intcode(intcode, input);
 
         assert_eq!(input, output);
     }
