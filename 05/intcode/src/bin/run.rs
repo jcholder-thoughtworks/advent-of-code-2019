9983b1b24e0f15ae3f8d6f6ea487614584c9ced4
JC Holder <jc.holder@thoughtworks.com>
2019-12-05 17:16:45 -0500

    Add #diagnose_thermal_radiators (not yet fully supported

--- 05/intcode/src/bin/run.rs
+++ 05/intcode/src/bin/run.rs
@@ -6,17 +6,29 @@ use intcode::*;
 fn main() -> io::Result<()> {
     let code = fs::read_to_string("input.txt")?;
 
-    diagnose_air_conditioner(code);
+    diagnose_air_conditioner(code.as_str());
+
+    diagnose_thermal_radiators(code.as_str());
 
     Ok(())
 }
 
-fn diagnose_air_conditioner(code: String) {
-    let mut program = Program::from(code.as_str());
+fn diagnose_air_conditioner(code: &str) {
+    let mut program = Program::from(code);
 
     println!("Diagnosing air conditioner ...");
 
     let output = program.execute(1);
+
+    println!("Output: {}", output);
+}
+
+fn diagnose_thermal_radiators(code: &str) {
+    let mut program = Program::from(code);
+
+    println!("Diagnosing thermal radiators ...");
+
+    let output = program.execute(5);
 
     println!("Output: {}", output);
 }
