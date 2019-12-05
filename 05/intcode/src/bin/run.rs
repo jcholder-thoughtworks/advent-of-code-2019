d4f6a6690e56862f6dcab4b5e3854c7b495aa2e0
JC Holder <jc.holder@thoughtworks.com>
2019-12-05 11:46:10 -0500

    Refactor run#main to use Program

--- 05/intcode/src/bin/run.rs
+++ 05/intcode/src/bin/run.rs
@@ -6,12 +6,14 @@ use intcode::*;
 fn main() -> io::Result<()> {
     let code = fs::read_to_string("input.txt")?;
     let original_intcode = parse_code(code);
+    let mut program = Program::new(original_intcode);
 
     let input = 0; // example input
 
     println!("Input: {}", input);
 
-    let (_executed_intcode, output) = execute_intcode(original_intcode, input);
+    let output = program.execute(input);
+
     println!("Output: {}", output);
 
     Ok(())
